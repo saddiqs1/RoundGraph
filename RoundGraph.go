9dfcda9e3bee2786add4ab6e716018d8aeb36bad
saddiqs1 <ssaddique96@hotmail.com>
2022-08-01 00:23:46 +0100

    Weighted graph, Overtime Scores (#2)
    * Change to weighted graph
    * refactored to RoundGraph
    * overtime scores done
    * check TODO for enhancement

---
+++ RoundGraph.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/path"
+	"gonum.org/v1/gonum/graph/simple"
+)
+
+type RoundGraph struct {
+	ids    map[Round]int64
+	rounds map[int64]Round
+	*simple.WeightedDirectedGraph
+}
+
+func newRoundGraph() RoundGraph {
+	return RoundGraph{
+		ids:                   make(map[Round]int64),
+		rounds:                make(map[int64]Round),
+		WeightedDirectedGraph: simple.NewWeightedDirectedGraph(0, 0),
+	}
+}
+
+func (g RoundGraph) addRound(r Round) {
+	n := g.NewNode()
+	id := n.ID()
+	n = node{
+		round: r,
+		id:    id,
+	}
+	g.AddNode(n)
+	g.ids[r] = id
+	g.rounds[id] = r
+}
+
+func (g RoundGraph) nodeAtRound(r Round) graph.Node {
+	id, ok := g.ids[r]
+	if !ok {
+		return nil
+	}
+	return g.WeightedDirectedGraph.Node(id)
+}
+
+func (g RoundGraph) roundAtId(id int64) Round {
+	return g.rounds[id]
+}
+
+func (g RoundGraph) idAtRound(r Round) int64 {
+	return g.ids[r]
+}
+
+type Round struct {
+	startTick   int
+	endTick     int
+	roundNumber int
+	t           int
+	ct          int
+	isHalfTime  bool
+}
+
+type node struct {
+	round Round
+	id    int64
+}
+
+type RoundStart struct {
+	tick int
+	t    int
+	ct   int
+}
+
+type ScoreUpdate struct {
+	tick  int
+	team  string
+	score int
+}
+
+func (n node) ID() int64   { return n.id }
+func (r Round) Total() int { return r.t + r.ct }
+
+func (g RoundGraph) setRounds(rounds []RoundStart, scores []ScoreUpdate, gameHalftimes []int, lastTick int) {
+	for i, round := range rounds {
+		r := Round{
+			startTick:   round.tick,
+			roundNumber: round.t + round.ct + 1,
+			t:           round.t,
+			ct:          round.ct,
+			isHalfTime:  false,
+		}
+
+		if int(i)+1 == len(rounds) {
+			r.endTick = lastTick
+		} else {
+			r.endTick = rounds[i+1].tick - 1
+		}
+
+		// Update round scores (some demos don't instantly update round scores when round is started)
+		for _, s := range scores {
+			if r.startTick <= s.tick && s.tick <= r.startTick+500 {
+				if s.team == "t" && s.score != r.t {
+					r.t = s.score
+				} else if s.team == "ct" && s.score != r.ct {
+					r.ct = s.score
+				}
+			}
+		}
+
+		// Set halftime bool using gameHalfEnded events
+		for _, gh := range gameHalftimes {
+			if r.startTick <= gh && gh <= r.endTick {
+				r.isHalfTime = true
+			}
+		}
+
+		// Some servers (e.g. esea) don't log gameHalfEnded events, so hard code it as below
+		if len(gameHalftimes) == 0 {
+			x := (r.roundNumber - 30) % 3 //TODO - check when the scores flip for the first time in OT to figure out it MR 6 or 10
+			if r.roundNumber == 15 {
+				r.isHalfTime = true
+			} else if r.roundNumber > 30 && x == 0 {
+				r.isHalfTime = true
+			}
+		}
+
+		g.addRound(r)
+	}
+}
+
+func (g RoundGraph) setEdges() {
+	nodes := g.Nodes()
+	for nodes.Len() > 0 {
+		nodes.Next()
+		n := g.Node(nodes.Node().ID()) // n = current node
+		cr := g.roundAtId(n.ID())      // get current round (cr)
+
+		// loop through rounds to attach edges
+		for _, r := range g.rounds {
+			/*
+				TODO:
+				Overtime rounds
+			*/
+			if cr.startTick < r.startTick { // cr tick must be before the next round in the graph to attach edge to it i.e. can't go back in time
+				if isHalfTime(cr, r) {
+					g.SetWeightedEdge(simple.WeightedEdge{F: n, T: g.nodeAtRound(r), W: 1})
+				} else if hasIncreasedByOne(cr, r) {
+					g.SetWeightedEdge(simple.WeightedEdge{F: n, T: g.nodeAtRound(r), W: float64(r.startTick - cr.startTick)})
+				}
+			}
+		}
+	}
+}
+
+func hasIncreasedByOne(r1, r2 Round) bool {
+	if r1.Total() == r2.Total()-1 { // Ensure round increased by 1
+		if r1.t == r2.t || r2.t == r1.t+1 { // Ensure t round only jumped up by 1
+			if r1.ct == r2.ct || r2.ct == r1.ct+1 { // Ensure ct round only jumped up by 1
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func isHalfTime(r1, r2 Round) bool {
+	if r1.isHalfTime { // if current round is halftime, then the rounds will switch next round
+		// t and ct will switch, one of them will +1 e.g. 4-10 becomes 11-4
+		if r1.t == r2.ct && r1.ct == r2.t-1 {
+			return true
+		}
+
+		if r1.ct == r2.t && r1.t == r2.ct-1 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (g RoundGraph) getMatchRounds() []Round {
+	startRounds := []Round{}
+	finalRound := Round{}
+	for _, r := range g.rounds {
+		//find all 0-0 rounds
+		if r.Total() == 0 {
+			startRounds = append(startRounds, r)
+		}
+
+		//find highest round
+		if finalRound.Total() < r.Total() {
+			finalRound = r
+		}
+	}
+
+	// Find the shortest path from each starting round
+	matchNodes := []graph.Node{}
+	var finalRoundsWeight float64
+	firstPass := true
+	pt := path.DijkstraAllPaths(g)
+	for _, startRound := range startRounds {
+		path, weight, _ := pt.Between(g.idAtRound(startRound), g.idAtRound(finalRound))
+		if len(path) > 0 {
+			if firstPass {
+				matchNodes = path
+				finalRoundsWeight = weight
+				firstPass = false
+			} else if weight < finalRoundsWeight {
+				matchNodes = path
+				finalRoundsWeight = weight
+			}
+		}
+	}
+
+	matchRounds := []Round{}
+	for _, rNode := range matchNodes {
+		matchRounds = append(matchRounds, g.roundAtId(rNode.ID()))
+	}
+
+	return matchRounds
+}
--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 	"github.com/joho/godotenv"
 	dem "github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs"
 	events "github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs/events"
-	"gonum.org/v1/gonum/graph"
-	"gonum.org/v1/gonum/graph/simple"
-	"gonum.org/v1/gonum/graph/traverse"
 )
 
 func main() {
@@ -27,28 +24,38 @@ func main() {
 	p := dem.NewParser(f)
 	defer p.Close()
 
-	// Create score graph
-	sg := newScoreGraph()
-
+	// Find all round start events
+	roundStarts := []RoundStart{}
 	p.RegisterEventHandler(func(e events.RoundStart) {
-		s := Score{
+		rs := RoundStart{
 			tick: p.GameState().IngameTick(),
 		}
 
-		s.t = p.GameState().TeamTerrorists().Score()
-		s.ct = p.GameState().TeamCounterTerrorists().Score()
+		rs.t = p.GameState().TeamTerrorists().Score()
+		rs.ct = p.GameState().TeamCounterTerrorists().Score()
 
-		//ensure there are no duplicates
-		scoreExists := false
-		for _, score := range sg.scores {
-			if score == s {
-				scoreExists = true
-				break
-			}
+		roundStarts = append(roundStarts, rs)
+	})
+
+	scoreUpdates := []ScoreUpdate{}
+	p.RegisterEventHandler(func(e events.ScoreUpdated) {
+		s := ScoreUpdate{
+			tick:  p.GameState().IngameTick(),
+			score: e.NewScore,
 		}
-		if !scoreExists {
-			sg.addScore(s)
+
+		if e.TeamState.Team() == 2 {
+			s.team = "t"
+		} else if e.TeamState.Team() == 3 {
+			s.team = "ct"
 		}
+
+		scoreUpdates = append(scoreUpdates, s)
+	})
+
+	gameHalftimes := []int{}
+	p.RegisterEventHandler(func(e events.GameHalfEnded) {
+		gameHalftimes = append(gameHalftimes, p.GameState().IngameTick())
 	})
 
 	// Parse to end
@@ -57,101 +64,20 @@ func main() {
 		log.Panic("failed to parse demo: ", err)
 	}
 
-	nodes := sg.Nodes()
-	for nodes.Len() > 0 {
-		nodes.Next()
-		n := sg.Node(nodes.Node().ID()) // n = current node
-		cs := sg.scoreAtId(n.ID())      // get current score (cs)
+	// Create round graph
+	rg := newRoundGraph()
 
-		// loop through scores to attach edges
-		for _, s := range sg.scores {
-			/*
-				TODO - EDGE CASES
-				Overtime
-			*/
-			if cs.tick < s.tick { // cs tick must be lower then the next score in the graph to attach edge to it i.e. can't go back in time
-				if isHalfTime(cs, s) {
-					sg.SetEdge(simple.Edge{F: n, T: sg.nodeAtScore(s)})
-				} else if hasIncreasedByOne(cs, s) {
-					sg.SetEdge(simple.Edge{F: n, T: sg.nodeAtScore(s)})
-				}
-			}
-		}
-	}
+	// Set rg.rounds
+	rg.setRounds(roundStarts, scoreUpdates, gameHalftimes, p.Header().PlaybackTicks)
 
-	startScores := []Score{}
-	finalScore := Score{}
+	// Set edges for each node
+	rg.setEdges()
 
-	for _, s := range sg.scores {
-		//find all 0-0 scores
-		if s.Total() == 0 {
-			startScores = append(startScores, s)
-		}
+	// Find the shortest path from the starting
+	matchRounds := rg.getMatchRounds()
 
-		//find highest score
-		if finalScore.Total() < s.Total() {
-			finalScore = s
-		}
-	}
-
-	//finding the longest possible path that exists, with the highest starting 0-0 score at the start
-	//TODO - WE NEED TO FIND LONGEST PATH
-	finalRounds := []Score{}
-	finalRounds = append(finalRounds, Score{})
-	for _, startScore := range startScores {
-		rounds := []Score{}
-		df := traverse.DepthFirst{
-			Visit: func(n graph.Node) {
-				// TODO - check if it's halftime node, do something
-				rounds = append(rounds, sg.scoreAtId(n.ID()))
-			},
-		}
-		df.Walk(sg, sg.nodeAtScore(startScore), func(n graph.Node) bool {
-			// until score is finalscore
-			return sg.scoreAtId(n.ID()) == finalScore
-		})
-
-		if len(finalRounds) < len(rounds) {
-			finalRounds = rounds
-		} else if len(finalRounds) == len(rounds) && finalRounds[0].tick < rounds[0].tick {
-			finalRounds = rounds
-		}
-	}
-
-	/*
-		TODO
-		- maybe change to weighted graph, change weights of edges based off how close the ticks are. Actually find longest path
-		- map these to Round struct, which includes start and end tick for the round
-	*/
 	// This is the set of rounds in a game...
-	for _, r := range finalRounds {
-		fmt.Printf("demo_goto %v, t = %v, ct = %v \n", r.tick, r.t, r.ct)
+	for _, r := range matchRounds {
+		fmt.Printf("demo_goto %v - demo_goto %v, r = %v, t = %v, ct = %v \n", r.startTick, r.endTick, r.roundNumber, r.t, r.ct)
 	}
-}
-
-func hasIncreasedByOne(s1 Score, s2 Score) bool {
-	if s1.Total() == s2.Total()-1 { // Ensure score increased by 1
-		if s1.t == s2.t || s2.t == s1.t+1 { // Ensure t score only jumped up by 1
-			if s1.ct == s2.ct || s2.ct == s1.ct+1 { // Ensure ct score only jumped up by 1
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-func isHalfTime(s1 Score, s2 Score) bool {
-	if s1.Total() == 14 { // if current total is 14, then the scores will switch next round
-		// t and ct will switch, one of them will +1 e.g. 4-10 becomes 11-4
-		if s1.t == s2.ct && s1.ct == s2.t-1 {
-			return true
-		}
-
-		if s1.ct == s2.t && s1.t == s2.ct-1 {
-			return true
-		}
-	}
-
-	return false
 }
